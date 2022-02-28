5aa30ba12e1b47816e927c05f6e5747f7b3f20e8
galeno <gui.galenocover@gmail.com>
2022-02-28 16:41:55 -0300

    ERB, Helpers and Methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   resources :coins
   root 'welcome#index'
+
+  get '/inicio', to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
